8513c58c8db138582258265017f8f60ebfd2fee3
askarakshabayev <askar.akshabayev@gmail.com>
2019-01-25 17:45:38 +0600

    Added comments

--- Lab1/Lab1/Program.cs
+++ Lab1/Lab1/Program.cs
@@ -6,7 +6,12 @@ namespace Lab1
     {
         static void Main(string[] args)
         {
+            // Comment 1
             int a = int.Parse(Console.ReadLine());
+            /*
+             * comment 1
+             * comment 2
+             */           
             int b = int.Parse(Console.ReadLine());
             Console.WriteLine(a + b + a + b);
         }
